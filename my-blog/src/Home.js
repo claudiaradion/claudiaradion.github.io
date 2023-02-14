69864ea97ad4d2327c44b05a17e4515f12b8161f
Claudia <radion_claudia@yahoo.com>
2023-02-14 21:46:36 +0200

    Configure React app for deployment to GitHub Pages

---
+++ my-blog/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="Home">
+        <h1>CLAUDIA</h1>
+        <p>I am a young girl who plays with different aspects of programming. <br/> Here will be the portfolio with the projects created by me. </p>
+    </div>
+  );
+}
+
+export default Home;
