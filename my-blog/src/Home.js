75aa80cfa5c674aba4a86e8ea0b79963734aab6f
Claudia <radion_claudia@yahoo.com>
2023-02-14 21:58:34 +0200

    Text modified

--- my-blog/src/Home.js
+++ my-blog/src/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="Home">
         <h1>CLAUDIA</h1>
-        <p>I am a young girl who plays with different aspects of programming. <br/> Here will be the portfolio with the projects created by me. </p>
+        <p>I am a girl who plays with different aspects of programming. <br/> Here will be the portfolio with the projects created by me. </p>
     </div>
   );
 }
