3d337d4bf1b34b453cd0ddf623d89b7ac007765e
iamclaudiar <59837410+iamclaudiar@users.noreply.github.com>
2023-07-04 22:23:56 +0300

    Update Home.js

--- my-blog/src/pages/Home.js
+++ my-blog/src/pages/Home.js
@@ -5,6 +5,8 @@ function Home() {
     <div className="TextDiv">
       <h1>CLAUDIA</h1>
       <p>I am a girl who plays with different aspects of programming. <br /> Here will be the portfolio with the projects created by me. </p>
+      <p>The application to keep track of friends' birthdays is one of the applications in my portfolio:</p>
+      <img src="https://github.com/iamclaudiar/FriendsBirthdays-FullStackApp/blob/main/BirthDateBook_App.PNG" alt="Web App Image">
     </div>
   );
 }
