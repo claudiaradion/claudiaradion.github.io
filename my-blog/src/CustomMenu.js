b736997b8f737b6cf96c93318186aa7933fb1a76
iamclaudiar <59837410+iamclaudiar@users.noreply.github.com>
2024-04-26 12:06:17 +0300

    Update CustomMenu.js

--- my-blog/src/CustomMenu.js
+++ my-blog/src/CustomMenu.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Blog from "./pages/Blog";
 
 function CustomMenu() {
@@ -14,7 +14,7 @@ function CustomMenu() {
                     <Route index element={<Home />} />
                     <Route path="blog" element={<Blog />} />
                     <Route path="contact" element={<Contact />} />
-                    <Route path="android" element={<NoPage />} />
+                    <Route path="android" element={<PrivacyPolicy />} />
                 </Route>
             </Routes>
         </BrowserRouter>
